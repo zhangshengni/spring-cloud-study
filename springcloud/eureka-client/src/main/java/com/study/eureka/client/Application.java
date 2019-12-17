475bbc8731345f3e26684e16d82f580cb2f12c25
zhangshengni <zhangshengni@163.com>
2019-12-17 18:36:37 +0800

    write code to add spring boot web
    write code to add spring boot web

--- springcloud/eureka-client/src/main/java/com/study/eureka/client/Application.java
+++ springcloud/eureka-client/src/main/java/com/study/eureka/client/Application.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
 @EnableEurekaClient
+@SpringBootApplication
 @RestController
 public class Application {
 
